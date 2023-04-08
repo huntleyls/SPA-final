2ffbf3633f1b5262685ac4e237554412739178c7
huntleyls <huntleyls@appstate.edu>
2023-04-07 23:14:12 -0400

    added components

--- final-project/src/app/app.module.ts
+++ final-project/src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ContactMeComponent,
+    ExperienceComponent
   ],
   imports: [
     BrowserModule
